a6863381d00fb34b9ceecce5f8e41d5a1ee22854
omri yakubovsky <omriya@cloudzone.io>
2022-06-24 10:52:23 +0300

    new model

--- sfatHalev/settings.py
+++ sfatHalev/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello_world',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'sfatHalev.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": ["sfatHalev/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
